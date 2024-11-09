8ed0d0d749cc832459182026ba08c18aae609fa9
Arthur Peixoto Melo <arthurpeixotomelo@gmail.com>
2024-11-09 02:34:09 -0300

    create data cleaning and analisys notebook

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from os import listdir
-from datetime import datetime
 from os.path import join, dirname, abspath
 
 import pandas as pd
@@ -34,7 +33,11 @@ def get_file_data(file, plant):
     if file.split('_')[0] == plant.title():
         with open(join(dirname(abspath(__file__)), 'data', file), 'r') as f:
             df = pd.read_csv(f)
-            df['Data'] = file.split('.')[0][-10:]
+            df['Data'] = pd.to_datetime(file.split('.')[0][-10:], format='%Y-%m-%d', yearfirst=True)
+            df['Data'] = df[['Data', 'Hora']].apply(
+                lambda x: x[0] + pd.DateOffset(1) if str(x[1]) >= str('00:00:00') 
+                and str(x[1]) < str('06:00:00') else x[0], axis=1
+            )
             return df[['Valor do sinal', 'Temperatura', 'Umidade', 'Data', 'Hora']]
         
 def concat_plant_data(plant):
@@ -43,22 +46,31 @@ def concat_plant_data(plant):
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 templates = Jinja2Templates(directory='templates')
 
 @app.get('/', response_class=HTMLResponse)
-def home(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse(request=request, name='index.html')
 
 @app.get('/plants/{plant}/info', response_class=HTMLResponse)
-def get_info(plant):
-    df = get_plant_data(plant, limit=1000)
+async def get_info(plant):
+    df = get_plant_data(plant, limit=5000)
     df['Temperatura'] = df['Temperatura'].apply(lambda x: float(x[:4]))
     df['Umidade'] = df['Umidade'].apply(lambda x: int(x[:2]))
     return f'''
-        <section id="info">
+        <section id='info'>
             <h3>Informações da planta</h3>
+            <div>
+                <button href='/plants/{plant}/graph' hx-get='/plants/{plant}/graph' hx-target='#content' hx-trigger='click' hx-swap='outerHTML' hx-indicator='#info'>
+                    Carregar Gráfico
+                </button>
+                <button href='/plants/{plant}/table' hx-get='/plants/{plant}/table' hx-target='#content' hx-trigger='click' hx-swap='outerHTML'
+                hx-indicator='#info'>
+                    Carregar Tabela
+                </button>
+            </div>
             <div>
                 <div>
                     <p>Nome: {plants_dict.get(plant).get('nome')}</p>
@@ -73,36 +85,39 @@ def get_info(plant):
                     <p>Valor do Sinal Máximo Registrado: {plants_dict.get(plant).get('signal').get('max')}</p>
                 </div>
             </div>
-            <div>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>Valor do sinal</th>
-                            <th>Temperatura</th>
-                            <th>Umidade</th>
-                            <th>Data</th>
-                            <th>Hora</th>
-                        </tr>
-                        {
-                            ''.join([
-                                f"""
-                                    <tr>
-                                        <td>{row[0]}</td>
-                                        <td>{row[1]}</td>
-                                        <td>{row[2]}</td>
-                                        <td>{row[3]}</td>
-                                        <td>{row[4]}</td>
-                                    </tr>
-                                """
-                                for row in zip(df['Valor do sinal'], df['Temperatura'], df['Umidade'], df['Data'], df['Hora'])
-                            ])
-                        }
-                    </tbody>
-                </table>
-            </div>
+            <div id='content' class='htmx-indicator'><b>Carregando...</b></div>
         </section>
     '''
 
+# @app.get('/plants/{plant}/graph', response_class=HTMLResponse)
+# async def get_graph(plant):
+#     df = get_plant_data(plant, limit=5000)
+#     df['Temperatura'] = df['Temperatura'].apply(lambda x: float(x[:4]) * 0.01)
+#     df['Umidade'] = df['Umidade'].apply(lambda x: int(x[:2]) * 0.01)
+#     # return
+    
+
+@app.get('/plants/{plant}/table', response_class=HTMLResponse)
+async def get_graph(plant):
+    df = get_plant_data(plant, limit=5000)
+    df['Temperatura'] = df['Temperatura'].apply(lambda x: float(x[:4]) * 0.01)
+    df['Umidade'] = df['Umidade'].apply(lambda x: int(x[:2]) * 0.01)
+    return f'''
+        <div id='content'>
+            <table>
+                <tbody>
+                    <tr><th>Valor do sinal</th><th>Temperatura</th><th>Umidade</th><th>Data</th><th>Hora</th></tr>
+                    {
+                        ''.join([
+                            f'<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td><td>{row[3]}</td><td>{row[4]}</td></tr>'
+                            for row in zip(df['Valor do sinal'], df['Temperatura'], df['Umidade'], df['Data'], df['Hora'])
+                        ])
+                    }
+                </tbody>
+            </table>
+        </div>
+    '''
+
 @app.get('/plants/{plant}/data')
-def get_data(plant):
-    return get_plant_data(plant).to_json(orient='records')
+async def get_data(plant):
+    return await get_plant_data(plant).to_json(orient='records')
